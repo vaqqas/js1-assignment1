84c33a9e4256cb5a23c88725ff6383c0aaa437bc
Vaqqas <m.vaqqas@gmail.com>
2024-05-05 20:39:54 -0400

    added boiler plate code

---
+++ answers/q1.js
@@ -0,0 +1,10 @@
+function run(input_number){
+    // let count = 0;
+    // for(let i=0; i<100; i++){
+    //     if(i % input_number == 0)
+    //         count++;
+    // }
+    // return count;
+}
+
+exports.run = run;
